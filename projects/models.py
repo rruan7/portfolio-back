430fa2d49c25817bb30d21f8cbfa5429d058995a
rruan7 <71851361+rruan7@users.noreply.github.com>
2022-03-20 10:21:13 -0700

    Update models.py

--- projects/models.py
+++ projects/models.py
@@ -5,10 +5,15 @@ from django.db import models
 def upload_path(instance, filename):
     return '/'.join([str(instance.title), filename])
 
+
 class Projects(models.Model):
+    CATEGORIES = (
+        ('applications', 'Applications'),
+        ('miniprojects', 'Mini-Projects'),
+    )
     title = models.CharField(max_length=120)
     description = models.TextField()
-    category = models.CharField(max_length=60, default="incomplete")
+    category = models.CharField(max_length=60, choices=CATEGORIES, default="applications")
     app_link = models.URLField(default="https://www.reneeruan.com")
     source_code = models.URLField(default="https://github.com/rruan7?tab=repositories")
     picture_link = models.URLField(default="https://user-images.githubusercontent.com/71851361/155896993-a3549ad7-fa6a-4822-baf5-dc9322a5997a.PNG")
